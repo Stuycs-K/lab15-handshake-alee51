c9ad57b7cb9f720d0e9d422cf607b2075ac2f581
Anastasia Lee <alee51@stuy.edu>
2025-01-03 15:33:15 -0500

    finished basic server and client

--- basic_client.c
+++ basic_client.c
@@ -6,4 +6,12 @@ int main() {
   int from_server;
 
   from_server = client_handshake( &to_server );
+  while (1) {
+    int rand_int;
+    int bytes = read(from_server, &rand_int, 4);
+    if (bytes <= 0) {
+      exit(0);
+    }
+    printf("%d\n", rand_int);
+  }
 }
--- basic_server.c
+++ basic_server.c
@@ -5,4 +5,14 @@ int main() {
   int from_client;
 
   from_client = server_handshake( &to_client );
+
+  int rand_int;
+  int r_file = open("/dev/random", O_RDONLY, 0);
+  while(1) {
+    read(r_file, &rand_int, 4);
+    rand_int = abs(rand_int) % 101;
+    // printf("rand_int = %d\n", rand_int);
+    write(to_client, &rand_int, 4);
+    sleep(1);
+  }
 }
