afb585bd8481927295e76a8c24e116e104cbd440
Anastasia Lee <alee51@stuy.edu>
2025-01-08 06:00:31 -0500

    it was the makefile...

--- forking_server.c
+++ forking_server.c
@@ -11,11 +11,10 @@ static void sighandler(int signo) {
 int main() {
   signal(SIGPIPE, SIG_IGN);
   signal(SIGINT, sighandler);
-  
+  int to_client;
+  int from_client;
 
   while (1) {
-    int to_client;
-    int from_client;
     from_client = server_setup();
     int f = fork();
     if (f == 0) { //subserver
@@ -24,13 +23,11 @@ int main() {
         char buff[256];
         while (1) {
             if (read(from_client, buff, sizeof(buff)) == -1) {
-                printf("hello\n");
                 break;
             }
             printf("received: %s\n", buff);
             sleep(1);
         }
-        exit(0);
     }
     close(to_client);
     close(from_client);
